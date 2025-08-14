1289702bc893d295cf2c3f2b254f88f934829634
cdegroote14 <christophe.degroote61@gmail.com>
2025-08-15 00:43:48 +0300

    Delete placeholder.ps1

--- placeholder.ps1
+++
@@ -1,8 +0,0 @@
-# Write "hello world" to the file
-"hello world" | Out-File -FilePath "C:\Windows\Temp\log_from_ps.txt" -Encoding UTF8
-
-# Sleep for 5 seconds
-Start-Sleep -Seconds 5
-
-# Overwrite the file with "junky word"
-"junky word" | Out-File -FilePath "C:\Windows\Temp\log_from_ps.txt" -Encoding UTF8
